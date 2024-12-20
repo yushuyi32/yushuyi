31843c9a2111069a2d24186412ae4a4246fac6d5
yushuyi <1553932749@qq.com>
2024-12-20 14:52:59 +0800

    test1220

--- ykdtest/case/test_ykdtestlogin.py
+++ ykdtest/case/test_ykdtestlogin.py
@@ -19,8 +19,8 @@ def test_01_login_success():
         # 发送POST请求
         response = requests.post(url, headers=headers, json=data)
 
-        # 检查响应状态码
-        if response.status_code == 200:
+        # 检查登录接口响应断言是否符合预期，是则登陆成功，保存token，否则返回登陆失败
+        if response.json().get('succ') == 1:
             jsondata = response.json()
             token = jsondata.get('result', {}).get('token')
 
@@ -32,8 +32,40 @@ def test_01_login_success():
                 print("登录失败：未获取到token")
                 return "登录失败：未获取到token"
         else:
-            print(f"登录失败: HTTP 状态码 {response.status_code}")
-            return f"登录失败: HTTP 状态码 {response.status_code}"
+            print(f"登录失败: 原因 {response.json().get('error')}")
+            return f"登录失败: 原因 {response.json().get('error')}"
+
+    except requests.exceptions.RequestException as e:
+        print(f"请求失败：{e}")
+        return f"请求失败：{e}"
+
+
+def test_02_login_failed_by_error_password():
+    # 读取Excel中的登录数据
+    data_res = my_read_excel("D:/VScode/workhome/test/ykdtest/data/ykdtest.xlsx", "login")
+    url = data_res[1][2]  # 获取URL
+    headers = ast.literal_eval(data_res[1][4])  # 安全解析请求头
+    data = ast.literal_eval(data_res[1][5])  # 安全解析请求参数
+
+    try:
+        # 发送POST请求
+        response = requests.post(url, headers=headers, json=data)
+
+        # 检查接口返回响应断言
+        if response.json().get('succ') == 1:
+            jsondata = response.json()
+            token = jsondata.get('result', {}).get('token')
+
+            if token:
+                print("登录成功，token：", token)
+                save_file(token=token)  # 保存token
+                return token
+            else:
+                print("登录失败：未获取到token")
+                return "登录失败：未获取到token"
+        else:
+            print(f"登录失败: 原因是{response.json().get('error')}")
+            return f"登录失败: 原因是{response.json().get('error')}"
 
     except requests.exceptions.RequestException as e:
         print(f"请求失败：{e}")
@@ -42,6 +74,7 @@ def test_01_login_success():
 
 if __name__ == '__main__':
     test_01_login_success()
+    test_02_login_failed_by_error_password()
 # import pytest
 # import requests
 # import os
